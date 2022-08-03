dbee37a5b146d535906ac5c3a1d1ef7acc172408
Vitor <vitor.leandro@edu.unifil.br>
2022-08-03 19:51:08 -0300

    Primeiro commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,17 @@ export class HomePage {
 
   constructor() {}
 
+  variavel_lista = [];
+  texto = "";
+  
+  adiciona() {
+    if (!(this.texto == "")) {
+      this.variavel_lista.push(this.texto);
+      this.texto = ""; 
+    } 
+    
+  }
+  //*ngFor = "let item of minhaLista" no item
+  //[(ngModel)]="texto" no input
+
 }
