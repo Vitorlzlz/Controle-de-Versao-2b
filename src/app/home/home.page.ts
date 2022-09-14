ab696955f0e5436ac74bd9776096ddc898c5460d
Vitorlzlz <104946344+Vitorlzlz@users.noreply.github.com>
2022-09-14 19:30:10 -0300

    uploud

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,64 +1,58 @@
-
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
   constructor(private storage:Storage) {
     this.storage.create();
   }
 
-  ngOnInit(){
+  variavel_lista = [];
+  texto: string = "";
+  valor: number = 0;
+  soma = 0;
+  aux = 0;
+
+ngOnInit(){
     this.atualizaLista();
   }
 
-  variavel_lista = [[]];
-  texto: string = "";
-  aux = 0;
-
-  async adiciona() {
-    if (!(this.texto == "")) {
-      //this.variavel_lista.push("0", this.texto);
-
-      this.variavel_lista.forEach(item => {
-        if(parseInt(item[0]) > this.aux) {
-          this.aux = parseInt(item[0]);
-        }
-      })
-      this.aux = this.aux + 1;
-      await this.storage.set(this.aux.toString(), this.texto);
-      this.atualizaLista();
-      this.texto = "";
-    }
-
-      /*
-    if (this.texto == "") {
-
-    } else{
-      this.variavel_lista.push(this.texto);
-      this.texto = "";
-    }*/
-
-  }
-
-  atualizaLista() {
+atualizaLista() {
     this.variavel_lista = [];
     this.storage.forEach((value, key, index) => {
+  console.log(index);
       this.variavel_lista.push([key, value]);
     })
+
+    this.somar();
   }
 
-  remove(indice) {
-    this.variavel_lista.splice(indice, 1)
+  async adiciona() {
+    if ( !(this.texto == "") && !(this.valor < 0)) {
+      this.variavel_lista.push(this.texto);
+      this.aux = this.variavel_lista.length - 1
+      await this.storage.set(this.aux.toString(), [this.texto, this.valor]);
+      this.atualizaLista();
+      this.valor = 0;
+      this.texto = "";
+    }
   }
 
-  //*ngFor = "let elemento_da_lista of minhaLista" no item
-  //[(ngModel)]="texto" no input
+  async remove(indice) {
+    await this.storage.remove(indice);
+    this.atualizaLista();
+  }
 
+  somar() {
+    this.soma = 0;
+    this.storage.forEach((value, key, index) => {
+      this.soma += value[1];
+    })
+  }
 }
